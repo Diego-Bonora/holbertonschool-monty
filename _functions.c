e83001bf32998da6b5b0e45ac041e4caa67a0983
Diego-Bonora <bonoradi@gmail.com>
2023-05-08 12:07:54 -0700

    file containing 2 functions called from the main program

---
+++ _functions.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _push - function for pushing a number to the stack
+ * @stack: head node of the stack
+ * @line_number: number to add to the new node
+ * Return: nothing
+ */
+
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = NULL;
+
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		fprintf(stderr, "%s", "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!*stack)
+	{
+		temp->n = line_number;
+		temp->prev = NULL;
+		temp->next = NULL;
+		*stack = temp;
+	}
+	else
+	{
+		temp->n = line_number;
+		temp->prev = NULL;
+		temp->next = *stack;
+		(*stack)->prev = temp;
+		*stack = temp;
+	}
+}
+
+/**
+ * _pall - function for printing all the stack
+ * @stack: head node of the stack
+ * @line_number: unnesesary variable
+ * Return: nothing
+ */
+
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+	if (*stack)
+	{
+		while (*stack != NULL)
+		{
+			printf("%d\n", (*stack)->n);
+			*stack = (*stack)->next;
+		}
+	}
+}
