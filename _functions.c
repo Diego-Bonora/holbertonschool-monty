d00e3a41502a8b73af5b4f9c90ddb09e9f17a5d5
Diego-Bonora <bonoradi@gmail.com>
2023-05-10 06:27:36 -0700

    function from main file moved into here

--- _functions.c
+++ _functions.c
@@ -79,3 +79,48 @@ char *_digit_checker(char *number)
 
 	return (number);
 }
+
+/**
+ * get_function - Function for getting the apropiate function
+ * @number: line read from the file
+ * @_opcode: its the opcode
+ * @stack: head node
+ * @line_count: number of line
+ * @line_number: parameter for the function
+ * Return: returns a function
+ */
+
+void (*get_function(char *number, char *_opcode, int line_count, int *error))(
+	stack_t **stack, unsigned int line_number)
+{
+	int count = 0;
+	instruction_t operators[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{NULL, NULL}
+	};
+	strtok(_opcode, " \n");
+	while (operators[count].opcode != NULL)
+	{
+		if (strcmp(operators[count].opcode, _opcode) == 0)
+		{
+			if (number && strcmp(_opcode, "push") == 0)
+			{
+				number = _digit_checker(number);
+				if ((!number))
+				{
+					fprintf(stderr, "L%d: usage: push integer\n", line_count);
+					*error = 1;
+					return (NULL);
+				}
+			}
+			return (operators[count].f);
+		}
+		count++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_count, _opcode);
+	*error = 1;
+	return (NULL);
+}
